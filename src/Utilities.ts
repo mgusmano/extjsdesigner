ac0afaf6dbb655468b78cd9040c9797ba2027475
mgusmano <mgusmano@yahoo.com>
2020-07-02 19:57:11 -0400

    Utilities cleanup

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -18,10 +18,10 @@ export class Utilities {
 
   public static monacoGenerated(nonce: string, webview: vscode.Webview, extensionPath: string): string {
     var e = `
-<script nonce="${nonce}" >var require = { paths: { 'vs': '${Utilities.getUriNode( webview, extensionPath, "monaco-editor/min/vs" )}' } };</script>
-<script nonce="${nonce}" src="${Utilities.getUriNode( webview, extensionPath, "monaco-editor/min/vs/loader.js" )}"></script>
-<script nonce="${nonce}" src="${Utilities.getUriNode( webview, extensionPath, "monaco-editor/min/vs/editor/editor.main.nls.js" )}"></script>
-<script nonce="${nonce}" src="${Utilities.getUriNode( webview, extensionPath, "monaco-editor/min/vs/editor/editor.main.js" )}"></script>
+    <script nonce="${nonce}" >var require = { paths: { 'vs': '${Utilities.getUriNode( webview, extensionPath, "monaco-editor/min/vs" )}' } };</script>
+    <script nonce="${nonce}" src="${Utilities.getUriNode( webview, extensionPath, "monaco-editor/min/vs/loader.js" )}"></script>
+    <script nonce="${nonce}" src="${Utilities.getUriNode( webview, extensionPath, "monaco-editor/min/vs/editor/editor.main.nls.js" )}"></script>
+    <script nonce="${nonce}" src="${Utilities.getUriNode( webview, extensionPath, "monaco-editor/min/vs/editor/editor.main.js" )}"></script>
     `
     return e;
   }
@@ -31,73 +31,41 @@ export class Utilities {
     var storeTemplate = ``;
     var viewModelTemplate = ``;
     var viewControllerTemplate = ``;
-
-    console.log("Open Tools or something else");
-
+    //console.log("Open Tools or something else");
     var Xtype = fileName.substring(last + 1, fileName.length - 3);
-    //var xtype = fileName.substring(last + 1, fileName.length - 3).toLowerCase()
     var XtypeFront = Xtype.substring(0, Xtype.length - 4);
-
     var prefix = fileName.substring(0, last + 1) + XtypeFront;
 
-    // console.log(this.messages)
-    // console.log(os)
-    // console.log(fileName)
-    // console.log(Xtype)
-    // console.log(xtype)
-    // console.log(XtypeFront)
-    // console.log(prefix)
-    //var view = prefix + 'View.js';
-    // var viewTemplate = ``
-    // try {
-    //   if (fs.existsSync(view)) {
-    //     const viewUri = webview.asWebviewUri(vscode.Uri.file(view));
-    //     viewTemplate = `<script nonce="${nonce}" src="${viewUri}"></script>`
-    //   }
-    // } catch(err) {}
-
     var storeModel = prefix + "StoreModel.js";
-
     try {
-        if (fs.existsSync(storeModel)) {
-            const storeModelUri = webview.asWebviewUri(
-                vscode.Uri.file(storeModel)
-            );
-            storeModelTemplate = `<script nonce="${nonce}" src="${storeModelUri}"></script>`;
-        }
+      if (fs.existsSync(storeModel)) {
+        const storeModelUri = webview.asWebviewUri( vscode.Uri.file(storeModel) );
+        storeModelTemplate = `<script nonce="${nonce}" src="${storeModelUri}"></script>`;
+      }
     } catch (err) {}
 
     var store = prefix + "ViewStore.js";
-
     try {
-        if (fs.existsSync(store)) {
-            const storeUri = webview.asWebviewUri(
-                vscode.Uri.file(store)
-            );
-            storeTemplate = `<script nonce="${nonce}" src="${storeUri}"></script>`;
-        }
+      if (fs.existsSync(store)) {
+        const storeUri = webview.asWebviewUri( vscode.Uri.file(store) );
+        storeTemplate = `<script nonce="${nonce}" src="${storeUri}"></script>`;
+      }
     } catch (err) {}
 
     var viewController = prefix + "ViewController.js";
-
     try {
-        if (fs.existsSync(viewController)) {
-            const viewControllerUri = webview.asWebviewUri(
-                vscode.Uri.file(viewController)
-            );
-            viewControllerTemplate = `<script nonce="${nonce}" src="${viewControllerUri}"></script>`;
-        }
+      if (fs.existsSync(viewController)) {
+        const viewControllerUri = webview.asWebviewUri( vscode.Uri.file(viewController) );
+        viewControllerTemplate = `<script nonce="${nonce}" src="${viewControllerUri}"></script>`;
+      }
     } catch (err) {}
 
     var viewModel = prefix + "ViewModel.js";
-
     try {
-        if (fs.existsSync(viewModel)) {
-            const viewModelUri = webview.asWebviewUri(
-                vscode.Uri.file(viewModel)
-            );
-            viewModelTemplate = `<script nonce="${nonce}" src="${viewModelUri}"></script>`;
-        }
+      if (fs.existsSync(viewModel)) {
+        const viewModelUri = webview.asWebviewUri( vscode.Uri.file(viewModel) );
+        viewModelTemplate = `<script nonce="${nonce}" src="${viewModelUri}"></script>`;
+      }
     } catch (err) {}
 
     return `
@@ -106,85 +74,48 @@ export class Utilities {
     ${viewModelTemplate}
     ${viewControllerTemplate}
     `
-
   }
 
-
-
-
   public static cmdGenerated(nonce: string, webview: vscode.Webview, last: any, fileName: string, separator: string): string {
-
     var cmdModelBaseTemplate = ``;
     var cmdModelPersonnelTemplate = ``;
     var cmdStorePersonnelTemplate = ``;
+    //console.log("Sencha Cmd Generated");
 
-
-    console.log("Sencha Cmd Generated");
-
-    var prefix =
-        fileName.substring(0, last + 1) +
-        ".." +
-        separator +
-        ".." +
-        separator;
-
-    console.log(prefix);
-
+    var prefix = fileName.substring(0, last + 1) + ".." + separator + ".." + separator;
     var cmdModelBase = prefix + "model" + separator + "Base.js";
-    console.log(cmdModelBase);
     try {
-        if (fs.existsSync(cmdModelBase)) {
-            console.log("model exists");
-            const u = webview.asWebviewUri(
-                vscode.Uri.file(cmdModelBase)
-            );
-            cmdModelBaseTemplate = `<script nonce="${nonce}" src="${u}"></script>`;
-        } else {
-            console.log("model does NOT exist");
-        }
-    } catch (err) {}
-    var cmdModelPersonnel =
-        prefix + "model" + separator + "Personnel.js";
-    console.log(cmdModelPersonnel);
-    try {
-        if (fs.existsSync(cmdModelPersonnel)) {
-            console.log("exists");
-            const u = webview.asWebviewUri(
-                vscode.Uri.file(cmdModelPersonnel)
-            );
-            cmdModelPersonnelTemplate = `<script nonce="${nonce}" src="${u}"></script>`;
-        }
-    } catch (err) {}
-    var cmdStorePersonnel =
-        prefix + "store" + separator + "Personnel.js";
-    console.log(cmdStorePersonnel);
-    try {
-        if (fs.existsSync(cmdStorePersonnel)) {
-            console.log("exists");
-            const u = webview.asWebviewUri(
-                vscode.Uri.file(cmdStorePersonnel)
-            );
-            cmdStorePersonnelTemplate = `<script nonce="${nonce}" src="${u}"></script>`;
-        }
+      if (fs.existsSync(cmdModelBase)) {
+        const u = webview.asWebviewUri( vscode.Uri.file(cmdModelBase) );
+        cmdModelBaseTemplate = `<script nonce="${nonce}" src="${u}"></script>`;
+      } else {
+        console.log("model does NOT exist");
+      }
     } catch (err) {}
 
+    var cmdModelPersonnel = prefix + "model" + separator + "Personnel.js";
+    try {
+      if (fs.existsSync(cmdModelPersonnel)) {
+        const u = webview.asWebviewUri( vscode.Uri.file(cmdModelPersonnel) );
+        cmdModelPersonnelTemplate = `<script nonce="${nonce}" src="${u}"></script>`;
+      }
+    } catch (err) {}
+
+    var cmdStorePersonnel = prefix + "store" + separator + "Personnel.js";
+    try {
+      if (fs.existsSync(cmdStorePersonnel)) {
+        const u = webview.asWebviewUri( vscode.Uri.file(cmdStorePersonnel) );
+        cmdStorePersonnelTemplate = `<script nonce="${nonce}" src="${u}"></script>`;
+      }
+    } catch (err) {}
     return `
     ${cmdModelBaseTemplate}
     ${cmdModelPersonnelTemplate}
     ${cmdStorePersonnelTemplate}
     `
-
   }
 
-
-// <script nonce="${nonce}" src="${Utilities.getUri( webview, extensionPath, "scripts/axios.js" )}"></script>
-// <script nonce="${nonce}" src="${Utilities.getUri( webview, extensionPath, "scripts/doAxios.js" )}"></script>
-// <script nonce="${nonce}" src="${Utilities.getUri( webview, extensionPath, "scripts/components.js" )}"></script>
-
-
-
   public static getScripts(nonce: string, webview: vscode.Webview, extensionPath: string, xtype: string, documenttext: string, extend: string): string {
-
     var e = `
     <script nonce="${nonce}" src="${Utilities.getUri(webview,extensionPath,"scripts/eventListenerMessage.js")}"></script>
     <script nonce="${nonce}" src="${Utilities.getUri(webview,extensionPath,"scripts/eventListenerLoad.js")}"></script>
